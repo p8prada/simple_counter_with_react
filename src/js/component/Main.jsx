3ed4263b82ee13e1b1b3169afc5a415c7f2bd181
p8prada <131833040+p8prada@users.noreply.github.com>
2023-06-19 06:35:48 +0000

    dots added

--- src/js/component/Main.jsx
+++ src/js/component/Main.jsx
@@ -5,20 +5,23 @@ import * as numbers from "../index.js"
 //create your first component
 const Main = () => {
 	return (
-		<div className="container-fluid ">
+	<div className="container-fluid ">
             <div className="row d-flex justify-content-center">
-
-                <i class="clock fa-regular fa-clock"></i>
-
+                <div className="col-1 mx-3 p-0">
+                        <i className="clock fa-regular fa-clock"></i>
+                </div>
+                <div className="col-1">
+                        <h1 className="dots">:</h1>
+                </div>
                     <SecondsCounter number={numbers.numberToShow[0]}></SecondsCounter>
                     <SecondsCounter number={numbers.numberToShow[1]}></SecondsCounter>
                     <SecondsCounter number={numbers.numberToShow[2]}></SecondsCounter>
                     <SecondsCounter number={numbers.numberToShow[3]}></SecondsCounter>
                     <SecondsCounter number={numbers.numberToShow[4]}></SecondsCounter>
                     <SecondsCounter number={numbers.numberToShow[5]}></SecondsCounter>
-            </div>
+                </div>
         </div>
-	);
+        );
 };
 
 export default Main;
